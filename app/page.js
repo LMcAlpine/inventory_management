db242c94d50b9f0ba4cbe617962be123483e8558
Luke <lukedmcalpine@gmail.com>
2024-08-04 21:51:56 -0700

    Add inventory item component

--- app/page.js
+++ app/page.js
@@ -105,7 +105,6 @@ export default function Home() {
       justifyContent="center"
       alignItems="center"
       gap={2}
-      /* bgcolor="#FF0000"*/
     >
       <AddItemModal open={open} onClose={handleClose} addItem={addItem} />
 
@@ -137,36 +136,6 @@ export default function Home() {
           addItem={addItem}
           removeItem={removeItem}
         />
-        {/* <Stack width="800px" height="300px" spacing={2} overflow="auto">
-          {inventory.map(({ name, quantity }) => (
-            <Box
-              key={name}
-              width="100%"
-              minHeight="150px"
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              bgColor="#f0f0f0"
-              padding={5}
-            >
-              <Typography variant="h3" color="#333" textAlign="center" flex={1}>
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </Typography>
-              <Typography variant="h3" color="#333" textAlign="center" flex={1}>
-                {quantity}
-              </Typography>
-              <Stack direction="row" spacing={2}>
-                <Button variant="contained" onClick={() => addItem(name)}>
-                  Add
-                </Button>
-
-                <Button variant="contained" onClick={() => removeItem(name)}>
-                  Remove
-                </Button>
-              </Stack>
-            </Box>
-          ))}
-        </Stack> */}
       </Box>
     </Box>
   );
---
+++ components/InventoryItem.js
@@ -0,0 +1,31 @@
+import { Box, Typography, Stack, Button } from "@mui/material";
+
+const InventoryItem = ({ name, quantity, addItem, removeItem }) => (
+  <Box
+    key={name}
+    width="100%"
+    minHeight="150px"
+    display="flex"
+    alignItems="center"
+    justifyContent="space-between"
+    bgColor="#f0f0f0"
+    padding={5}
+  >
+    <Typography variant="h3" color="#333" textAlign="center" flex={1}>
+      {name.charAt(0).toUpperCase() + name.slice(1)}
+    </Typography>
+    <Typography variant="h3" color="#333" textAlign="center" flex={1}>
+      {quantity}
+    </Typography>
+    <Stack direction="row" spacing={2}>
+      <Button variant="contained" onClick={() => addItem(name)}>
+        Add
+      </Button>
+
+      <Button variant="contained" onClick={() => removeItem(name)}>
+        Remove
+      </Button>
+    </Stack>
+  </Box>
+);
+export default InventoryItem;
